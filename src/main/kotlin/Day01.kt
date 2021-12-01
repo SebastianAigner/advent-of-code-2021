b523d61f91d48c6d0ac83c48101883d056ede6e5
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2021-12-01 06:14:37 +0100

    Initial commit

---
+++ src/main/kotlin/Day01.kt
@@ -0,0 +1,20 @@
+package day01
+
+import java.io.File
+
+val input = File("inputs/day01.txt").readLines().map { it.toInt() }
+
+fun main() {
+    println(
+        input
+            .windowed(2)
+            .count { (a, b) -> b > a }
+    )
+    println(
+        input
+            .windowed(3)
+            .map { it.sum() }
+            .windowed(2)
+            .count { (a, b) -> b > a }
+    )
+}
