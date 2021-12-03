efc3798b1aef4ff9f6a6fa65420f5c4b2c57539d
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2021-12-03 07:07:27 +0100

    Day3 extracted HOF instead of code duplication

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -12,6 +12,11 @@ fun List<String>.charactersForColumn(n: Int): Map<Char, Int> {
 }
 
 fun main() {
+    part1()
+    part2()
+}
+
+fun part1() {
     val charFrequencyByColumn = input[0].indices.map { column ->
         input.charactersForColumn(column)
     }
@@ -21,64 +26,40 @@ fun main() {
     }
     val invertedNumber = combined.map { if(it == '0') '1' else '0' }.joinToString("")
     println(combined.toInt(2) * invertedNumber.toInt(2))
-    part2()
 }
 
 fun part2() {
-    var dynInput = input
-    for(column in input[0].indices) {
-        val charFrequencyByColumn = dynInput.charactersForColumn(column)
-        val zeroes = charFrequencyByColumn['0'] ?: 0
-        val ones = charFrequencyByColumn['1'] ?: 0
+    val oxyGenRating = input.foo { list, column, zeroes, ones ->
         val popular =
             when {
                 zeroes > ones ->'0'
                 zeroes == ones -> '1'
                 else ->'1'
             }
-        dynInput = dynInput.filter { it[column] == popular }
-        if(dynInput.size == 1) break
+        list.filter { it[column] == popular }
     }
 
-    val oxyGenRating = dynInput.single()
-
-
-    dynInput = input
-    for(column in input[0].indices) {
-        val charFrequencyByColumn = dynInput.charactersForColumn(column)
-        val zeroes = charFrequencyByColumn['0'] ?: 0
-        val ones = charFrequencyByColumn['1'] ?: 0
+    val co2ScrubberRating = input.foo { list, column, zeroes, ones ->
         val popular =
             when {
                 zeroes > ones ->'1'
                 zeroes == ones -> '0'
                 else ->'0'
             }
-        dynInput = dynInput.filter { it[column] == popular }
-        if(dynInput.size == 1) break
+        list.filter { it[column] == popular }
     }
 
-    val co2ScrubberRating = dynInput.single()
     println(oxyGenRating.toInt(2) * co2ScrubberRating.toInt(2))
 }
 
-fun part22() {
-    var outstr = ""
-    var dynInput = input
-    for(idx in input[0].indices) {
-        var zeroes = 0
-        var ones = 0
-        for(number in dynInput) {
-            if(number[idx] == '0') zeroes++ else ones++
-        }
-        val unpopular =
-            when {
-                zeroes == ones -> '0'
-                zeroes > ones ->'1'
-                else ->'0'
-            }
-        dynInput = dynInput.filter { it[idx] == unpopular }
-        println(dynInput)
-        if(dynInput.size == 1) println("SDLKFJSDLKFJDLKFJD ${dynInput[0].toInt(2)}")
+fun List<String>.foo(filter: (list: List<String>, column: Int, zeroes: Int, ones: Int) -> List<String>): String {
+    var dynInput = this
+    for(column in this[0].indices) {
+        val charFrequencyByColumn = dynInput.charactersForColumn(column)
+        val zeroes = charFrequencyByColumn['0'] ?: 0
+        val ones = charFrequencyByColumn['1'] ?: 0
+        dynInput = filter(dynInput, column, zeroes, ones)
+        if(dynInput.size == 1) break
     }
+    return dynInput.joinToString("")
 }
