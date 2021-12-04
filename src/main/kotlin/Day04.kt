f44985bb9dec3a984fe3deab27aaeb6ceac16626
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2021-12-04 07:25:45 +0100

    Day04

---
+++ src/main/kotlin/Day04.kt
@@ -0,0 +1,81 @@
+package day04
+
+import java.io.File
+
+val input = File("inputs/day04.txt").readLines()
+val draws = input[0].split(",").map { it.toInt() }
+val boards = input
+    .drop(1)
+    .chunked(6)
+    .map { singleBoardLines ->
+        singleBoardLines.filter { singleBoardLine ->
+            singleBoardLine.isNotBlank()
+        }
+    }
+
+fun main() {
+    val cleanedInput = boards.map { board ->
+        board.map { line ->
+            line.split(" ", "  ")
+                .filter { it.isNotBlank() } // Leading whitespace
+                .map { it.toInt() }
+        }
+    }
+
+    var bingoBoards = cleanedInput.map { BingoBoard.fromCollection(it) }
+
+    // Prints all winners in order of appearance.
+    // Part 1 is the first winner.
+    // Part 2 is the last winner.
+    for (currentDraw in draws) {
+        for (board in bingoBoards) {
+            board.mark(currentDraw)
+            if (board.checkBoard()) {
+                val sumOfUnmarkedFields = board.unmarked().sum()
+                println(sumOfUnmarkedFields * currentDraw)
+                bingoBoards = bingoBoards.filter { it != board } // remove winners
+            }
+        }
+    }
+}
+
+data class BingoField(val value: Int, val marked: Boolean = false)
+
+data class BingoBoard(val fields: List<MutableList<BingoField>>) {
+
+    private val widthIndices = fields[0].indices
+    private val heightIndices = fields.indices
+
+    companion object {
+        fun fromCollection(coll: List<List<Int>>): BingoBoard {
+            return BingoBoard(coll.map { row -> row.map { field -> BingoField(field) }.toMutableList() })
+        }
+    }
+
+    fun checkBoard() = checkRow() || checkColumn()
+    private fun checkRow() = fields.any { row -> row.all { it.marked } }
+
+    private fun checkColumn(): Boolean {
+        for (column in widthIndices) {
+            var rowOk = true
+            for (row in heightIndices) {
+                if (!fields[row][column].marked) {
+                    rowOk = false
+                    continue
+                }
+            }
+            if (rowOk) return true
+        }
+        return false
+    }
+
+    fun mark(num: Int) {
+        for (row in this.fields) {
+            row.replaceAll {
+                if (it.value == num) it.copy(marked = true) else it
+            }
+        }
+    }
+
+    fun unmarked() = fields.flatten().filter { !it.marked }.map { it.value }
+}
