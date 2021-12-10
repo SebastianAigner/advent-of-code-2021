674cdca6601075394befe432ed53cfd7d61f2021
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2021-12-10 06:50:14 +0100

    Day10 refactor

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -5,31 +5,45 @@ import java.io.File
 val input = File("inputs/day10.txt").readLines()
 
 fun main() {
-    println(input.sumOf {
-        val syntaxError = firstIllegalCharacter(it)
-        if (syntaxError == null) 0 else points[syntaxError.got]!!
+    val parseResult = input.map { parseLine(it) }
+    part1(parseResult)
+    part2(parseResult)
+}
+
+fun part1(results: List<ParseResult>) {
+    val points = mapOf(
+        ')' to 3,
+        ']' to 57,
+        '}' to 1197,
+        '>' to 25137
+    )
+
+    println(results.filterIsInstance<SyntaxError>().sumOf {
+        points.getValue(it.got)
     })
-    val incompleteLines = input.filter { firstIllegalCharacter(it) == null }
-    println(incompleteLines[0])
-    val total = incompleteLines.map {
-        val fillerChars = completeLine(it)
-        var lineScore = 0L
-        for (char in fillerChars) {
-            lineScore *= 5
-            lineScore += part2Score[char]!!
+}
+
+fun part2(results: List<ParseResult>) {
+    val points = mapOf(
+        ')' to 1,
+        ']' to 2,
+        '}' to 3,
+        '>' to 4
+    )
+
+    val total = results.filterIsInstance<Autocomplete>().map {
+        it.missingChars.fold(0L) { acc, c ->
+            acc * 5 + points.getValue(c)
         }
-        lineScore
-    }.sorted().also { println(it) }
+    }.sorted()
 
     println(total[total.size / 2])
 }
 
-val points = mapOf(
-    ')' to 3,
-    ']' to 57,
-    '}' to 1197,
-    '>' to 25137
-)
+sealed class ParseResult
+
+data class SyntaxError(val expected: Char?, val got: Char) : ParseResult()
+data class Autocomplete(val missingChars: List<Char>) : ParseResult()
 
 val mapping = mapOf(
     '(' to ')',
@@ -38,48 +52,18 @@ val mapping = mapOf(
     '<' to '>'
 )
 
-// [[)
-// [  (])
-// [[ (]])
-//
-
-data class SytaxError(val expected: Char?, val got: Char)
-
-fun firstIllegalCharacter(line: String): SytaxError? {
+fun parseLine(line: String): ParseResult {
     val deque = ArrayDeque<Char>()
     for (char in line) {
-        println("Looking at $char with $deque")
-        val isOpeningBracket = char in mapping.keys
-        if (isOpeningBracket) deque.addLast(mapping[char]!!)
+        val isOpening = char in mapping.keys
+        if (isOpening) deque.addLast(mapping.getValue(char))
         else {
             val corresponding = deque.removeLastOrNull()
             if (corresponding != char) {
                 //mismatched pair
-                return SytaxError(corresponding, char)
+                return SyntaxError(corresponding, char)
             }
         }
     }
-    return null
-}
-
-val part2Score = mapOf(
-    ')' to 1,
-    ']' to 2,
-    '}' to 3,
-    '>' to 4
-)
-
-fun completeLine(line: String): List<Char> {
-    val deque = ArrayDeque<Char>()
-    for (char in line) {
-        println("Looking at $char with $deque")
-        val isOpeningBracket = char in mapping.keys
-        if (isOpeningBracket) {
-            deque.addLast(mapping[char]!!)
-        } else {
-            val corresponding = deque.removeLastOrNull()
-            if (corresponding != char) error("you  didn't filter right!")
-        }
-    }
-    return deque.reversed()
+    return Autocomplete(deque.reversed())
 }
