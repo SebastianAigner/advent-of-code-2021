921c3fc77e4df73700543753a7f93158599e98cd
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2021-12-24 07:54:39 +0100

    Day24 dp approach (too slow)

--- src/main/kotlin/Day24.kt
+++ src/main/kotlin/Day24.kt
@@ -32,14 +32,35 @@ data class Mod(val targetRegister: String, val value: String) : Instruction()
 data class Eql(val targetRegister: String, val value: String) : Instruction()
 
 fun main() {
+    val chunks = mutableListOf<List<Instruction>>()
+    var curr = mutableListOf<Instruction>()
+    for (instr in input) {
+        if (instr is Inp) {
+            // beginninig of a new chunk
+            chunks += curr
+            curr = mutableListOf(instr)
+        } else {
+            curr += instr
+        }
+    }
+    chunks.removeAt(0)
     println(input)
+    val blocks = chunks.map { Block(it) }
+
+    println(chunks)
+
+    for (inputNum in 9 downTo 1) {
+        val reggy = interpretByBlocks(listOf(blocks[0]), inputNum.toString())
+        println(reggy)
+    }
+
     for (inputNum in 99999999999999L downTo 0L) {
-        if (inputNum % 100000L == 0L) {
+        if (inputNum % 10000000L == 0L) {
             println(inputNum)
         }
         val inp = StringBuilder(inputNum.toString())
         if (inp.contains('0')) continue
-        val interpreted = interpret(input, inp)
+        val interpreted = interpretByBlocks(blocks, inputNum.toString())
         if (interpreted.isValid()) {
             println(inputNum)
             return
@@ -47,16 +68,61 @@ fun main() {
     }
 }
 
-fun Map<String, Int>.isValid(): Boolean {
-    return this["z"] == 0
+fun Map<String, Long>.isValid(): Boolean {
+    return this["z"] == 0L
 }
 
-fun interpret(instructions: List<Instruction>, input: StringBuilder): Map<String, Int> {
-    val registers = mutableMapOf<String, Int>()
-    fun lookup(value: String): Int {
-        val num = value.toIntOrNull()
+fun Registers.isValid(): Boolean {
+    return z == 0L
+}
+
+data class Block(val l: List<Instruction>) {
+    val hashCode = l.hashCode()
+
+    override fun hashCode(): Int {
+        return hashCode
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return l == other
+    }
+}
+
+var cache = mutableMapOf<Block, Registers>()
+fun interpretByBlocks(blocks: List<Block>, input: String): Registers {
+    var y: Long = 0L
+    var z: Long = 0L
+    for ((idx, block) in blocks.withIndex()) {
+        val cachedRegg = cache[block] ?: run {
+            val res = interpret(block.l, input.drop(idx), mutableMapOf("y" to y, "z" to z))
+            val reggs = Registers(res["y"]!!, res["z"]!!)
+            cache.put(block, reggs)
+            reggs
+        }
+
+        y = cachedRegg.y
+        z = cachedRegg.z
+        if (z != 0L) {
+            return Registers(y, z)
+        }
+    }
+    return Registers(y, z)
+}
+
+data class Registers(val y: Long, val z: Long)
+
+fun interpret(
+    instructions: List<Instruction>,
+    inputString: String,
+    registers: MutableMap<String, Long>,
+): MutableMap<String, Long> {
+    val input = StringBuilder(inputString)
+    val inputString = null
+    fun lookup(value: String): Long {
+        val num = value.toLongOrNull()
         if (num != null) return num
-        return registers[value] ?: 0
+        if (value == "z") println(registers[value])
+        return registers[value] ?: 0L
     }
     for (inst in instructions) {
         when (inst) {
@@ -72,7 +138,7 @@ fun interpret(instructions: List<Instruction>, input: StringBuilder): Map<String
                 registers[inst.targetRegister] = if (registers[inst.targetRegister] == lookup(inst.value)) 1 else 0
             }
             is Inp -> {
-                registers[inst.register] = input.takeDeleting(1).toInt()
+                registers[inst.register] = input.takeDeleting(1).toLong()
             }
             is Mod -> {
                 val curr = registers[inst.targetRegister] ?: 0
